5d544c7fd50ebab86669509286b7a1fc8a96bf8c
Shahar Vaknin <shaharvaknin909@gmail.com>
2022-03-07 00:43:48 +0200

    show all posts on app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { getAllPosts, selectPosts } from "../src/store/posts";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const posts = useSelector(selectPosts);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={() => {
+            console.log(posts);
+            dispatch(getAllPosts());
+          }}
         >
-          Learn React
-        </a>
+          load all posts
+        </button>
+        <p>
+          {posts.map((post) => (
+            <>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+              <p>{new Date(post.date).toLocaleDateString()}</p>
+            </>
+          ))}
+        </p>
       </header>
     </div>
   );

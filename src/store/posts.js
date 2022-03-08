ad6173d28b7ba71252c7d3ac5f8f8f4d089143ce
Shahar Vaknin <shaharvaknin909@gmail.com>
2022-03-09 00:18:00 +0200

    support translateAllPosts on redux

--- src/store/posts.js
+++ src/store/posts.js
@@ -1,23 +1,50 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-
-import { fetchAllPosts } from "./networking";
+import { fetchAllPosts, translateText } from "./networking";
 
 export const getAllPosts = createAsyncThunk("posts/getAllPosts", async () => {
   const response = await fetchAllPosts();
   return response.data;
 });
 
+export const translateAllPosts = createAsyncThunk(
+  "posts/translate",
+  async ({ posts, currentLanguage, languageTranslateTo }) => {
+    const requestBody = posts.map(({ body }) => ({ text: body }));
+    const { data } = await translateText(
+      currentLanguage.value,
+      languageTranslateTo.value,
+      requestBody
+    );
+    const translatedPosts = posts.map((post, index) => ({
+      ...post,
+      body: data[index].translations[0].text,
+    }));
+
+    return { allPosts: translatedPosts, currentLanguage: languageTranslateTo };
+  }
+);
+
 export const postsSlice = createSlice({
   name: "posts",
-  initialState: [],
+  initialState: {
+    allPosts: [],
+    currentLanguage: { label: "English", value: "EN" },
+  },
   reducers: {
-    updatePosts: (stare, action) => action.payload,
+    updatePosts: (_stare, action) => action.payload,
   },
   extraReducers: (builder) => {
-    builder.addCase(getAllPosts.fulfilled, (state, action) => action.payload);
+    builder.addCase(getAllPosts.fulfilled, (state, action) => ({
+      ...state,
+      currentLanguage: { label: "English", value: "EN" },
+      allPosts: action.payload,
+    }));
+    builder.addCase(
+      translateAllPosts.fulfilled,
+      (state, action) => action.payload
+    );
   },
 });
 
-export const selectPosts = (state) => {
-  return state.posts;
-};
+export const selectPosts = (state) => state.posts.allPosts;
+export const selectCurrentLanguage = (state) => state.posts.currentLanguage;

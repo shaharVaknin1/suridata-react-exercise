08fb87930c5e614624da6b03966df04ac3b86845
Shahar Vaknin <shaharvaknin909@gmail.com>
2022-03-09 00:17:25 +0200

    create generic styles file

---
+++ src/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: #4caf50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  width: 100%;
+  &:hover {
+    cursor: pointer;
+  }
+`;

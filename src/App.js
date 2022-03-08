d1745e71c26f383fbbfe85671ebeb4571dc54bf3
Shahar Vaknin <shaharvaknin909@gmail.com>
2022-03-09 00:18:35 +0200

    use all of the new things on App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import { useSelector, useDispatch } from "react-redux";
-import { getAllPosts, selectPosts } from "../src/store/posts";
-import "./App.css";
+import Select from "react-select";
+import { AllPosts } from "./components/postsContainer";
+import {
+  translateAllPosts,
+  selectCurrentLanguage,
+  getAllPosts,
+  selectPosts,
+} from "../src/store/posts";
+import { Button } from "./styles";
+import * as config from "./config.json";
+
+const { sportedLanguages } = config;
 
 function App() {
   const dispatch = useDispatch();
   const posts = useSelector(selectPosts);
+  const currentLanguage = useSelector(selectCurrentLanguage);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button
-          onClick={() => {
-            console.log(posts);
-            dispatch(getAllPosts());
-          }}
-        >
-          load all posts
-        </button>
-        <p>
-          {posts.map((post) => (
-            <>
-              <h1>{post.title}</h1>
-              <p>{post.body}</p>
-              <p>{new Date(post.date).toLocaleDateString()}</p>
-            </>
-          ))}
-        </p>
-      </header>
-    </div>
+    <>
+      <Button
+        onClick={() => {
+          dispatch(getAllPosts());
+        }}
+      >
+        Load All Posts
+      </Button>
+      <Select
+        value={currentLanguage}
+        options={sportedLanguages}
+        onChange={(selectedOption) =>
+          dispatch(
+            translateAllPosts({
+              posts,
+              currentLanguage,
+              languageTranslateTo: selectedOption,
+            })
+          )
+        }
+      />
+      <AllPosts />
+    </>
   );
 }
 
